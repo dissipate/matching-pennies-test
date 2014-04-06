b1949198301bffd3b6312ada9e3e2991b223195c
Steven Kane <dissipate@gmail.com>
2014-04-06 12:33:52 -0700

    added empty stubs for empty directories, added gitignore for hidden lein files

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject matching-pennies-test "0.1.0-SNAPSHOT"
-  :description "Matching pennies game server daemon"
+  :description "Matching Pennies game server daemon"
   :url "https://github.com/dissipate/matching-pennies-test"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
