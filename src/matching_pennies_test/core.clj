95f159d55bb17123f94b6df18833879dd3dbde01
Steven Kane <dissipate@gmail.com>
2014-05-14 00:59:33 -0700

    Defined 'valid-round-choices' for 'determine-round-result'

--- src/matching_pennies_test/core.clj
+++ src/matching_pennies_test/core.clj
@@ -1,7 +1,9 @@
 (ns matching-pennies-test.core)
 
+(def valid-round-choices #{'h' 't'})
+
 (defn determine-round-result [{:keys [matcher-choice mismatcher-choice]}]
-  {:pre [(contains? #{'h' 't'} matcher-choice), (contains? #{'h' 't'} mismatcher-choice)]
+  {:pre [(contains? valid-round-choices matcher-choice), (contains? valid-round-choices mismatcher-choice)]
    :post [(or (= % {:matcher 'w' :mismatcher 'l'}) (= % {:matcher 'l' :mismatcher 'w'}))] }
   "Maps matcher's choice and mismatcher's choice to win/loss outcome"
        (if (= matcher-choice mismatcher-choice)
